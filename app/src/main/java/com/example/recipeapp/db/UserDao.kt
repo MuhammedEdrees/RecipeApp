c46388aabf1f5f1f8aa0b1dba3e320fb88d9f43d
Edrees <mohamededrees234@hotmail.com>
2023-08-01 16:22:22 +0300

    added hasUsername and verifyPassword methods to UserDao

--- app/src/main/java/com/example/recipeapp/db/UserDao.kt
+++ app/src/main/java/com/example/recipeapp/db/UserDao.kt
@@ -2,11 +2,18 @@ package com.example.recipeapp.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.recipeapp.auth.model.User
 
 @Dao
 interface UserDao {
     @Insert
     suspend fun insertUser(vararg users: User)
+
+    @Query("SELECT EXISTS(SELECT * FROM users WHERE username = :username)")
+    suspend fun hasUsername(username: String)
+
+    @Query("SELECT EXISTS(SELECT * FROM users WHERE username = :username and password = :password)")
+    suspend fun verifyPassword(username: String, password: String)
 
 }
