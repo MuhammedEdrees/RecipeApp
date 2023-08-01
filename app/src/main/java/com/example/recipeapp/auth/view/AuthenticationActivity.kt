6c98dd434df54e6a930b4c2b4fe3ebe3ee1fd4dc
Edrees <mohamededrees234@hotmail.com>
2023-08-01 13:12:53 +0300

    added packages to auth package

--- app/src/main/java/com/example/recipeapp/auth/view/AuthenticationActivity.kt
+++ app/src/main/java/com/example/recipeapp/auth/view/AuthenticationActivity.kt
@@ -1,11 +1,12 @@
-package com.example.recipeapp
+package com.example.recipeapp.auth.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recipeapp.R
 
-class MainActivity : AppCompatActivity() {
+class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_authentication)
     }
 }
