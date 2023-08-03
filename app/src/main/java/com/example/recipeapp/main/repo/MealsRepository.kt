13468f37a8bb594fe80b902f9da608f4e018a68d
remon551 <65865953+remon551@users.noreply.github.com>
2023-08-03 17:25:04 +0300

    implemented the repository pattern for the local data source

--- app/src/main/java/com/example/recipeapp/main/repo/MealsRepository.kt
+++ app/src/main/java/com/example/recipeapp/main/repo/MealsRepository.kt
@@ -1,9 +1,13 @@
 package com.example.recipeapp.main.repo
 
+import com.example.recipeapp.main.model.Meal
 import com.example.recipeapp.main.model.MealResponse
 
 interface MealsRepository {
     suspend fun getMealsResponseByFirstLetter(letter: Char): MealResponse
     suspend fun getRandomMeal(): MealResponse
     suspend fun search(name: String): MealResponse
+    suspend fun insertMeal(meal: Meal)
+    suspend fun deleteMeal(meal: Meal)
+    suspend fun getMealById(mealID: String): Meal
 }
