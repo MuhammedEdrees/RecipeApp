bbd0c2846fb48134b89c33e3e27d96cfb44411a4
remon551 <65865953+remon551@users.noreply.github.com>
2023-08-08 17:45:58 +0300

    DetailsFragment

--- app/src/main/java/com/example/recipeapp/main/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/com/example/recipeapp/main/viewmodel/DetailsViewModel.kt
@@ -10,15 +10,18 @@ import com.example.recipeapp.main.repo.FavoriteRepository
 import com.example.recipeapp.main.repo.MealsRepository
 import kotlinx.coroutines.launch
 
-class DetailsViewModel (private val mealRepo: MealsRepository,
-                        private val favoriteRepo: FavoriteRepository
-) : ViewModel() {
+class DetailsViewModel(mealRepo: MealsRepository,
+                       favoriteRepo: FavoriteRepository) : RecipeViewModel(mealRepo, favoriteRepo) {
     private val _meal = MutableLiveData<Meal>()
     val meal: LiveData<Meal> = _meal
     fun getRemoteMeal(mealID: String){
         viewModelScope.launch{
             _meal.value = mealRepo.getRemoteMealById(mealID).meals.first()
-            Log.d("edrees ->", "Response: ${_meal.value}")
+        }
+    }
+    fun checkIfFavorite(mealId: String){
+        viewModelScope.launch{
+            _isFavorite.value = mealRepo.checkIfFavorite(mealId)
         }
     }
 }
